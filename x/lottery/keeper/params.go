517967d8b185146ea8c88f06945567f2436647ca
Developer Experience team at Tendermint <hello@tendermint.com>
2020-11-28 23:30:01 +0700

    Initialized with Starport

---
+++ x/lottery/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ppalineee/lottery/x/lottery/types"
+)
+
+// GetParams returns the total set of lottery parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the lottery parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
